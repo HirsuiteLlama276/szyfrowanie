390613db7d49168d4b85737e16b05a32fe1b8a77
HirsuiteLlama276 <60354361+HirsuiteLlama276@users.noreply.github.com>
2021-06-10 12:04:43 +0200

    Add files via upload

---
+++ deszyfracjaplotowego.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+string deszyfruj(string tekst)
+{
+    string wynik;
+    int dl = tekst.size();
+    for (int i = dl; i > 0; i =- 4) wynik = wynik + tekst[i];
+    for (int i = dl-1; i > 0; i =- 2) wynik = wynik + tekst[i];
+    for (int i = dl-2; i > 0; i =- 4) wynik = wynik + tekst[i];
+    return wynik;
+}
+
+int main()
+{
+    string tekst;
+    cout << "Wpisz tekst do odszyfrowania" << endl;
+    cin >> tekst;
+    cout << "Twoj odszyfrowany komunikat: " << deszyfruj(tekst) << endl;
+}
